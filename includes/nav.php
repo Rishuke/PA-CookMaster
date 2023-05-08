84e3263b9ae3b0c78a52385ff22253d2f2d5c698
Spéro <kotin.spero98@gmail.com>
2023-05-08 18:19:09 +0200

    Add inscription.php

--- includes/nav.php
+++ includes/nav.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-darklight">
             <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="#!">Wicookin</a>
+                <a class="navbar-brand" href="{#!">Wicookin</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Home</a></li>
                         <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
@@ -26,19 +26,22 @@
                         &nbsp;
                         &nbsp;
                         &nbsp;
-                        <button class="btn btn-outline-dark" type="submit">
-                            
-                            &nbsp; Inscription
-                           
+                        
+                        <button class="btn btn-outline-dark">
+                            <a href="inscription.php">
+                                 Inscription
+                            </a>
                         </button>
-                         &nbsp;
+                        
                         &nbsp;
                         &nbsp;
                         &nbsp;
-                        <button class="btn btn-outline-dark" type="submit">
-                            
-                            &nbsp; Connexion
-                           
+                        &nbsp;
+                        
+                        <button class="btn btn-outline-dark">
+                            <a href="connexion.php"> 
+                                Connexion
+                            </a>
                         </button>
                     </form>
                 </div>
