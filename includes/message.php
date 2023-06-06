23e3ecbcb711f59114235eb35dd8f7e7fa5795b2
Spéro <kotin.spero98@gmail.com>
2023-06-06 22:42:52 +0200

    "Remove all files"

--- includes/message.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if(isset($_GET['message']) && !empty($_GET['message']) && isset($_GET['type'])){
-    echo '<div class="alert alert-' . $_GET['type'] . '" role="alert">' . htmlspecialchars($_GET['message']) . '</div>';
-}
