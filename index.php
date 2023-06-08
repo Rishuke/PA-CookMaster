719e037b9689bd735315b412cf6b6963d5787f97
Spéro <kotin.spero98@gmail.com>
2023-06-08 02:15:35 +0200

    Modify router in index.php

--- index.php
+++ index.php
@@ -2,30 +2,37 @@
 
 
 // https://www.wicookin.fr
-// https://www.wicookin.fr/users/ => // https://www.wicookin.fr/index.php
+// https://www.wicookin.fr/members/ => // https://www.wicookin.fr/index.php
 
 
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
 
-if (isPath("users")) {
+require_once __DIR__ . "/libraries/path.php";
+require_once __DIR__ . "/libraries/method.php";
+require_once __DIR__ . "/libraries/response.php";
+
+
+if (isPath("members")) {
     if (isGetMethod()) {
-        require_once __DIR__ . "/routes/users/get.php";
+        require_once __DIR__ . "/routes/members/get.php";
         die();
     }
 
     if (isPostMethod()) {
-        require_once __DIR__ . "/routes/users/post.php";
+        require_once __DIR__ . "/routes/members/post.php";
         die();
     }
 }
 
-if (isPath("users/:user")) {
+if (isPath("members/:member")) {
     if (isDeleteMethod()) {
-        require_once __DIR__ . "/routes/users/delete.php";
+        require_once __DIR__ . "/routes/members/delete.php";
         die();
     }
 
     if (isPatchMethod()) {
-        require_once __DIR__ . "/routes/users/patch.php";
+        require_once __DIR__ . "/routes/members/patch.php";
         die();
     }
 }
