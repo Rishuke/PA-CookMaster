53517d2ee241707af33ccf52f78bc47a4b3a2b3b
Spéro <kotin.spero98@gmail.com>
2023-06-20 00:46:46 +0200

    Add views for MVC

---
+++ views/auth/login.php
@@ -0,0 +1,37 @@
+<section id="" class="contact mt-5 pt-5">
+      <div class="container">
+
+        <div class="section-title">
+          <h2><span>Connexion</span></h2>
+        </div>
+      </div>
+
+      <div class="container mt-5">
+
+        <form action="" method="post" role="form" class="php-email-form">
+          <div class="row">
+
+            <div class="form-group" >
+              <label>Email</label>
+              <input type="email" name="email" id="email" placeholder="Votre email" class="form-control" required>
+            </div>
+            <div class=" form-group ">
+              <label>Mot de passe</label>
+              <input type="password" name="password" placeholder="Votre mot de passe" class="form-control" required>
+            </div>
+          
+           
+          
+          </div>
+          
+          
+          <div class="my-3">
+            <div class="loading">Loading</div>
+            <div class="error-message"></div>
+            <div class="sent-message">Your message has been sent. Thank you!</div>
+          </div>
+          <div class="text-center"><button type="submit">Se connecter</button></div>
+        </form>
+
+      </div>
+    </section>
